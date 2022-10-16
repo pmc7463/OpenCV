8a31e2688a0fd50f1b71906acb3c1c827e14a1a3
changstudent <akscjfals@naver.com>
2022-10-16 23:15:41 +0900

    01.variable

---
+++ Chap03/01.variable.py
@@ -0,0 +1,20 @@
+from numpy import var
+
+
+variable1 = 100     # 정수 변수 선언
+variable2 = 3.14    # 실수 변수 선언
+variable3 = -200    # 정수 변수 선언
+variable4 = 1.2 + 3.4j # 복수수 변수 선언
+variable5 = 'This is Python'    # 문자열 변수 선언
+variable6 = True    # bool 변수 선언
+variable7 = float(variable1)    # 자료형 변경
+variable8 = int(variable2)
+
+print('v1 = ', variable1, type(variable1))  # 변수의 값과 자료형 출력
+print('v2 = ', variable2, type(variable2))  # 변수의 값과 자료형 출력
+print('v3 = ', variable3, type(variable3))  # 변수의 값과 자료형 출력
+print('v4 = ', variable4, type(variable4))  # 변수의 값과 자료형 출력
+print('v5 = ', variable5, type(variable5))  # 변수의 값과 자료형 출력
+print('v6 = ', variable6, type(variable6))  # 변수의 값과 자료형 출력
+print('v7 = ', variable7, type(variable7))  # 변수의 값과 자료형 출력
+print('v8 = ', variable8, type(variable8))  # 변수의 값과 자료형 출력
